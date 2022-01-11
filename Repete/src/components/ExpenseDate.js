c5c12e0191342f591ab36c6c34c8f5346c060f42
BlackratQQ <vojtech.kochta@gmail.com>
2022-01-11 19:51:55 +0100

    Repete

---
+++ Repete/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const day = props.date.toLocaleString("en-EU", { month: "long" });
+  const month = props.date.toLocaleString("en-EU", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date_year">{year}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
